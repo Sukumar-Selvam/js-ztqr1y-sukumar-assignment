b0ce048087f01ade3d4468fbb3e42983bdf15853
Sukumar-Selvam <117559973+Sukumar-Selvam@users.noreply.github.com>
2022-12-31 10:40:52 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Abcd');
+//1. Print JS output in 4 ways
+console.log('Js Console Output');
+const appId = document.getElementById('app');
+appId.innerHTML('Document');
+document.write('Document Output');
+//alert('Js Alert Output');
+
+//2. Variable Hoisting Example ?
+//3. Function Hoisting Vs IIFE example ?
+//4. Example of SetInterval Callback function with clear operation
+//5. Example Of Making private variables using Closures
+//6. Perform Basic arithmetic operation such as Add,Sub,Mul,Divide with NAN validation
+//7. Print Title Case of given String
+//8. Sort ["Xavier","Utopiar"] array of string to Ascending and Descending order and also return as colon separated strings
+//9. Validate email address of string
+//10. Sort Array of numbers & Array of strings
+//11. Print Random numbers between 0 to 56
+//12. Array insertAtFirst,insertAtLast,insertAtMiddle using mutable way
+//13. Array deleteAtFirst,deleteAtLast,deleteAtMiddle using immutable way
+//14. Example to transform an array of strings into Title Case and Sort Them using Method Chaining
